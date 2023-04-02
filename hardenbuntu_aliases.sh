84b4fd79ece6222995881bee7b5e5eba673add00
sydhds <sydhds@gmail.com>
2023-04-02 13:01:11 +0200

    Add notes about hbu_chromium && hbu_chromium_paranoid aliases in browser section

--- hardenbuntu_aliases.sh
+++ hardenbuntu_aliases.sh
@@ -5,6 +5,21 @@ alias hbu_up="sudo apt update && sudo apt upgrade \
     sudo fwupdmgr update"
 alias hbu_chromium='/snap/bin/chromium \
 	--js-flags="--jitless" \
+	--enable-strict-mixed-content-checking \
+	--enable-strict-powerful-feature-restrictions \
+	--site-per-process \
+    --disable-3d-apis \
+	--disable-gpu \
+ 	--disable-webgl \
+	--disable-webgl2 \
+    --disable-accelerated-2d-canvas \
+    --no-vr-runtime \
+    --disable-speech \
+    --disable-speech-api \
+    --disable-dinosaur-easter-egg \
+    '
+alias hbu_chromium_paranoid='/snap/bin/chromium \
+	--js-flags="--jitless" \
 	--enable-potentially-annoying-security-features \
 	--enable-strict-mixed-content-checking \
 	--enable-strict-powerful-feature-restrictions \
@@ -19,4 +34,9 @@ alias hbu_chromium='/snap/bin/chromium \
     --disable-speech-api \
     --disable-dinosaur-easter-egg \
 	--disable-webrtc-hw-encoding \
-	--disable-webrtc-hw-decoding'
+	--disable-webrtc-hw-decoding \
+    --disable-accelerated-video-decode \
+    --disable-file-system \
+    --disable-reading-from-canvas \
+    --disable-background-networking \ 
+    '
