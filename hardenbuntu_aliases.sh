f4d7b6971b821a163acfeb36b389603c684e9f5a
sydhds <sydhds@gmail.com>
2023-10-09 20:40:40 +0200

    Add brave browser aliases

--- hardenbuntu_aliases.sh
+++ hardenbuntu_aliases.sh
@@ -41,3 +41,25 @@ alias hbu_chromium_paranoid='type hbu_chromium_paranoid; /snap/bin/chromium \
     --disable-reading-from-canvas \
     --disable-background-networking \
     '
+alias hbu_brave_paranoid='type hbu_brave_paranoid; /snap/bin/brave \
+	--js-flags="--jitless" \
+	--enable-strict-mixed-content-checking \
+	--enable-strict-powerful-feature-restrictions \
+	--site-per-process \
+    --disable-3d-apis \
+	--disable-gpu \
+ 	--disable-webgl \
+	--disable-webgl2 \
+    --disable-accelerated-2d-canvas \
+    --no-vr-runtime \
+    --disable-speech \
+    --disable-speech-api \
+    --disable-dinosaur-easter-egg \
+	--disable-webrtc-hw-encoding \
+	--disable-webrtc-hw-decoding \
+	--enable-potentially-annoying-security-features \
+    --disable-accelerated-video-decode \
+    --disable-file-system \
+    --disable-reading-from-canvas \
+    --disable-background-networking \
+    '
